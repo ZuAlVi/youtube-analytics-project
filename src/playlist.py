bc989f2ce702d08d7511d09a86723f88274003bd
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-27 13:35:19 +0300

    Attributes playlist_id, playlist_videos and playlist_info became private

--- src/playlist.py
+++ src/playlist.py
@@ -13,24 +13,24 @@ class PlayList:
 
     def __init__(self, playlist_id):
         """Экземпляр инициализируется id плейлиста. Дальше все данные будут подтягиваться по API."""
-        self.playlist_id = playlist_id
-        self.playlist_videos = PlayList.youtube.playlistItems().list(playlistId=playlist_id,
-                                                                     part='snippet, contentDetails, id, status',
-                                                                     maxResults=50,
-                                                                     ).execute()
-        self.playlist_info = PlayList.youtube.playlists().list(part='snippet',
-                                                               id=playlist_id,
-                                                               maxResults=50,
-                                                               ).execute()
-        self.title = self.playlist_info['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
+        self.__playlist_id = playlist_id
+        self.__playlist_videos = PlayList.youtube.playlistItems().list(playlistId=playlist_id,
+                                                                       part='snippet, contentDetails, id, status',
+                                                                       maxResults=50,
+                                                                       ).execute()
+        self.__playlist_info = PlayList.youtube.playlists().list(part='snippet',
+                                                                 id=playlist_id,
+                                                                 maxResults=50,
+                                                                 ).execute()
+        self.title = self.__playlist_info['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/playlist?list={self.__playlist_id}"
 
     @property
     def total_duration(self):
         """Метод возвращает объект класса `datetime.timedelta`
         с суммарной длительность плейлиста
         (обращение как к свойству, использовать `@property`)"""
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.__playlist_videos['items']]
         video_response = PlayList.youtube.videos().list(part='contentDetails,statistics',
                                                         id=','.join(video_ids)
                                                         ).execute()
@@ -45,7 +45,7 @@ class PlayList:
     def show_best_video(self):
         """Метод возвращает ссылку на самое популярное видео
          из плейлиста (по количеству лайков)"""
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.playlist_videos['items']]
+        video_ids: list[str] = [video['contentDetails']['videoId'] for video in self.__playlist_videos['items']]
         temp_like = 0
         temp_url = ''
         for item in video_ids:
