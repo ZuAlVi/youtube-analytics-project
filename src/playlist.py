a1d8960b7e68e7f856698a2df4af1bba94121dd7
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-27 13:23:07 +0300

    Added __init__ method in class PlayList

--- src/playlist.py
+++ src/playlist.py
@@ -6,3 +6,16 @@ from googleapiclient.discovery import build
 class PlayList:
     __api_key: str = os.getenv('YT_API_KEY')
     youtube = build('youtube', 'v3', developerKey=__api_key)
+
+    def __init__(self, playlist_id):
+        self.playlist_id = playlist_id
+        self.playlist_videos = PlayList.youtube.playlistItems().list(playlistId=playlist_id,
+                                                                     part='snippet, contentDetails, id, status',
+                                                                     maxResults=50,
+                                                                     ).execute()
+        self.playlist_info = PlayList.youtube.playlists().list(part='snippet',
+                                                               id=playlist_id,
+                                                               maxResults=50,
+                                                               ).execute()
+        self.title = self.playlist_info['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
