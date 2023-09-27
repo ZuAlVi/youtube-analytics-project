aa45c737f7311835377d1d4a7c32c6998b08716b
Alexandr Zubarev <zualvi@yandex.ru>
2023-09-27 13:21:45 +0300

    Crated class PlayList in playlist.py

--- src/playlist.py
+++ src/playlist.py
@@ -1,4 +1,8 @@
+import os
+
+from googleapiclient.discovery import build
 
 
-
-
+class PlayList:
+    __api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=__api_key)
