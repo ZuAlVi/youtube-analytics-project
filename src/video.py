5db814b874fa7d2b3fccc79ab364ce6073b48162
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-02 17:10:30 +0300

    Changed __init__ method in video.py

--- src/video.py
+++ src/video.py
@@ -1,6 +1,7 @@
 import os
 
 from googleapiclient.discovery import build
+from src.errors import InvalidIDError
 
 
 class Video:
@@ -8,19 +9,32 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=__api_key)
 
     def __init__(self, video_id):
-        """Экземпляр инициализируется id видео. Дальше все данные будут подтягиваться по API."""
-        self.video_id = video_id
-        self.video_response = Video.youtube.videos().list(
-            part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.url = f'https://youtu.be/{self.video_id}'
-        self.view_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        """Экземпляр инициализируется id видео. Если id существует,
+        то дальше все данные будут подтягиваться по API.
+        Если нет, то атрибутам title, url, view_count и like_count
+        будет присвоено значение None"""
+        try:
+            self.video_id = video_id
+            self.video_response = Video.youtube.videos().list(
+                part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
+            if not self.video_response['items']:
+                raise InvalidIDError
+        except InvalidIDError:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+            # print('Не верный ID видео')
+        else:
+            self.title = self.video_response['items'][0]['snippet']['title']
+            self.url = f'https://youtu.be/{self.video_id}'
+            self.view_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
         """Метод возвращает информацию об объекте для пользователя
         return: '<название канала>'"""
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
@@ -29,3 +43,5 @@ class PLVideo(Video):
         Данные будут подтягиваться из родительского класса Video."""
         super().__init__(video_id)
         self.playlist_id = playlist_id
+
+
